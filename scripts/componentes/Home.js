15a4643efd1e04caa9c4605debb36b18fb9d33af
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-05-09 12:43:30 -0300

    se agregan recetas al data

--- scripts/componentes/Home.js
+++ scripts/componentes/Home.js
@@ -1,15 +1,58 @@
 Vue.component('home',{
   data:function(){
     return{
-          dato_padre:"DATO DEL PADRE",
-          elemento:"Dato inicial",
+          libroDeCocina: [
+            {
+              nombre:"panqueques",
+              categoria:"dulces",
+              ingredientes:[
+                {"nombre":"huevo", "cantidad": "3", "unidad":"Unidades"},
+                {"nombre":"azúcar Hileret Light", "cantidad": "1", "unidad":"cucharada"},
+                {"nombre":"lehe", "cantidad": "1", "unidad":"taza"},
+                {"nombre":"harina 0000", "cantidad": "1", "unidad":"taza"},
+                {"nombre":"sal", "cantidad": "1", "unidad":"pizca"}
+              ],
+              preparacion: `Mezclar los huevos con el azúcar Hileret Light, incorporar la leche y la pizca de sal.
+              Por último, agregar de a poco la harina para que no se formen grumos.
+              Poner a calentar en una sartén o panquequera una cucharada de manteca, esparcir por toda la sartén para que el panqueque no se pegue.
+              Luego agregar la mezcla con un cucharón (la cantidad depende del espesor que te guste) y cocinar hasta que veas que se despegan los bordes y se dora, dar vuelta y terminar la cocción.
+              Algunas opciones de relleno pueden ser clásico con dulce de leche sin azúcar, mermelada de duraznos light con peras frescas, frutos rojos y todas las que te animes a probar.`
+            },
+            {
+              nombre:"salsa de hongos de pino",
+              categoria:"salados",
+              ingredientes: [
+                {"nombre": "cebolla", "cantidad": "1", "unidad": "unidad"},
+                {"nombre": "manteca", "cantidad": "25", "unidad": "gr"},
+                {"nombre": "aceite", "cantidad": "2", "unidad": "cucharada"},
+                {"nombre": "harina", "cantidad": "1", "unidad": "cucharada"},
+                {"nombre": "agua caliente", "cantidad": "1", "unidad": "taza"},
+                {"nombre": "hongos de pino", "cantidad": "30", "unidad": "gr"},
+                {"nombre": "perejil", "cantidad": "1", "unidad": "cucharada"},
+                {"nombre": "jugo de limon", "cantidad": "1", "unidad": "cucharada"},
+                {"nombre": "sal", "cantidad": "15", "unidad": "gr"},
+                {"nombre": "pimienta", "cantidad": "1", "unidad": "cucharadita"},
+                {"nombre": "mostaza", "cantidad": "1", "unidad": "cucharada"}
+              ],
+              preparacion: `Poner a remojar en la taza de agua los hongos y picar finamente el prejil.
+              Colocar en una sartén la manteca y el aceite. Cuando estén calientes, dorar la cebolla y agregar la harina; añadir revolver un rato y cuando esté todo cocido, añadir el jugo de limón, los hongos con el agua en que han estado en remojo, el perejil, y dejar cocinar 5 ó 10 minutos; si quedara  muy espesa,, agregar un poco más de caldo o agua.
+              Nota: Esta salsa es muy rica para acompañar carnes o arroz blanco.
+              `
+            }
+          ],
+          usuario: "Mabel",
+
           }
   },
   template:
   `
     <div id="padre">
-      <h2>Componente Principal</h2>
-        <p>Valor del padre: <span> {{ dato_padre }} </span></p>
-        <p>Variable de padre/hijo: <span> {{ elemento}} </span></p>
+      <div class="row">
+        <div class="col-12">
+          <h2>Bienvenida {{usuario}} a tu libro de recetas</h2>
+        </div>
+      </div>
+
+
     </div>`
 });
